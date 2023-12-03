29991aa0c00ffcb945fbe79800a6620d65e93439
Marcos Oliveira <marcosfatec2014@gmail.com>
2023-12-03 13:57:51 -0300

    File structure and basic settings

---
+++ marketpy/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+  title="MarketPy",
+  version="0.1.0",
+  description="Just an e-commerce in Python",
+)
---
+++ setup.py
@@ -0,0 +1,36 @@
+import io
+import os
+from setuptools import find_packages, setup
+
+def read(*paths, **kwargs):
+  content = ""
+  with io.open(
+    os.path.join(os.path.dirname(__file__), *paths),
+    encoding=kwargs.get("encoding", "utf8"),
+  ) as open_file:
+    content = open_file.read().strip()
+  return content
+
+def read_requirements(path):
+  return[
+    line.strip()
+    for line in read(path).split("\n")
+    if not line.startswith(('"', "#", "-", "git+"))
+  ]
+
+setup(
+  name="marketpy",
+  version="0.1.0",
+  description="Just an e-commerce in Python",
+  url="marketpy.io",
+  python_requires=">=3.8",
+  long_description="Just an e-commerce in Python",
+  long_description_content_type="text/markdown",
+  author="Marcos Oliveira",
+  packages=find_packages(exclude=["tests"]),
+  include_package_data=True,
+  install_requires=read_requirements("requirements.txt"),
+  entry_points={
+    "console_scripts": ["pamps = pamps.cli:main"]
+  }
+)
